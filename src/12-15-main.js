82491ce6efcf7ad85ff277ac0dbbf4e903501941
vvnha <vvnha.17it3@sict.udn.vn>
2022-07-14 16:52:26 +0700

    update

--- src/12-15-main.js
+++ src/12-15-main.js
@@ -27,11 +27,54 @@ function createBinarySearchTree(initKeyList) {
     return node;
   }
 
+  function findMin(node) {
+    if (node === null) return null;
+
+    let currentNode = node;
+    while (currentNode.left !== null) {
+      currentNode = currentNode.left;
+    }
+
+    return currentNode;
+  }
+
+  function search(node, key) {
+    if (node === null) return null;
+
+    if (key > node.key) return search(node.right, key);
+    if (key < node.key) return search(node.left, key);
+
+    return node;
+  }
+
+  function getMaxNodesCount(node) {
+    if (node === null) return 0;
+
+    const leftNodesCount = getMaxNodesCount(node.left);
+    const rightNodesCount = getMaxNodesCount(node.right);
+
+    return leftNodesCount > rightNodesCount ? leftNodesCount + 1 : rightNodesCount + 1;
+  }
+
+  function getHeight() {
+    const maxNodesCount = getMaxNodesCount(root);
+
+    return maxNodesCount > 0 ? maxNodesCount - 1 : 0;
+  }
+
   return {
     root,
     insert,
+    findMin,
+    search,
+    getMaxNodesCount,
+    getHeight,
   };
 }
 
 const binarySearchTree = createBinarySearchTree([10, 7, 15]);
 console.log(binarySearchTree.root);
+console.log(binarySearchTree.findMin(binarySearchTree.root));
+console.log(binarySearchTree.search(binarySearchTree.root, 15));
+console.log(binarySearchTree.getMaxNodesCount(binarySearchTree.root));
+console.log(binarySearchTree.getHeight());
